a6b1ae0baf65529b913a02c78722b0cb9ff06284
lisatostrams <lmc.tostrams@student.ru.nl>
2016-12-19 09:31:46 +0100

    finalized analysis

--- data.py
+++ data.py
@@ -13,6 +13,6 @@ def load_json(path):
         data = load(f)
     return data
     
-data = load_json('Data/amsterdam.json')
+data = load_json('Data/amsterdam (2).json')
 for i in range(0,3):
     print(data[i]['_id'])
